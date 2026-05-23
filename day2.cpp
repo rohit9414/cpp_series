ce9443e923b0a064b47621e0365e5331d7f91717
rohit9414 <rohit6201164441@gmail.com>
2026-05-23 12:13:30 +0000

    day3 completed

--- day2.cpp
+++ day2.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"hello";
+    cout<<"hello\n";
+    int age =25;
+    float pi =3.14f;/*for floating value we write f otherwise it take as double */
+    char ch='a';
+    double d=3.14;//8bytes //by default floating point value is double
+    bool isAdult=true;// true converted to 1 and false converted to 0
+    cout<<sizeof(age)<<endl;//4bytes
+    cout<<sizeof(pi)<<endl;//4bytes
+    cout<<sizeof(ch)<<endl;//1byte
+    cout<<sizeof(isAdult)<<endl;//1byte
+
+    //typecasting there are two types of typecasting in c++ 
+    //1. implicit typecasting-> done by compiler automatically
+    // code example
+    int a=10;
+    float b=a;// implicit typecasting from int to float
+    cout<<b<<endl;//10.0
+    //2. explicit typecasting-> done by programmer using cast operator
+    float c=3.14f;
+    int d1=(int)c;// explicit typecasting from float to int
+    cout<<d1<<endl;//3
     return 0;
 }
  
---
+++ day3.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  /* //input
+    int num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    //output
+    cout << "Sum: " << num1 + num2 << endl;
+    */
+   //arithmetic operators
+   /* int a=10, b=3;
+    cout<<"a + b = "<<a+b<<endl;//13
+    cout<<"a - b = "<<a-b<<endl;//7
+    cout<<"a * b = "<<a*b<<endl;//30
+    cout<<"a / b = "<<a/b<<endl;//3
+     cout<<"a / b = "<<a/(float)b<<endl;//3
+    cout<<"a % b = "<<a%b<<endl;//1
+    */
+   //relational operators
+   /*  int x=10, y=20;
+    cout<<"x == y: "<<(x==y)<<endl;// x == y: 0
+    cout<<"x != y: "<<(x!=y)<<endl;// x != y: 1
+    cout<<"x > y: "<<(x>y)<<endl;// x > y: 0
+    cout<<"x < y: "<<(x<y)<<endl;// x < y: 1
+    cout<<"x >= y: "<<(x>=y)<<endl;// x >= y: 0
+    cout<<"x <= y: "<<(x<=y)<<endl;// x <= y: 1
+    */
+   /*
+    //logical operators-> used to combine multiple conditions
+    // &&-> logical AND operator, ||-> logical OR operator, !-> logical NOT operator
+     bool p=true, q=false;
+    cout<<"p && q: "<<(p&&q)<<endl;// p && q: 0
+    cout<<"p || q: "<<(p||q)<<endl;// p || q: 1
+    cout<<"!p: "<<(!p)<<endl;// !p: 0
+    cout<<"!q: "<<(!q)<<endl;// !q: 1
+    */
+   //unary operators-> used to perform operations on a single operand
+// ++-> increment operator, ---> decrement operator, - -> unary minus operator
+ //code example
+   /* int i=5;
+    cout<<"i: "<<i<<endl;// i: 5
+    cout<<"++i: "<<++i<<endl;// ++i: 6
+    cout<<"i++: "<<i++<<endl;// i++: 6
+    cout<<"i: "<<i<<endl;// i: 7
+    cout<<"--i: "<<--i<<endl;// --i: 6
+    cout<<"i--: "<<i--<<endl;// i--: 6
+    cout<<"i: "<<i<<endl;// i: 5  
+    //prefix increment operator(++i) increments the value of i before using it in the expression, while postfix increment operator(i++) uses the current value of i in the expression and then increments it.
+    //prefix decrement operator(--i) decrements the value of i before using it in the expression, while postfix decrement operator(i--) uses the current value of i in the expression and then decrements it.
+    //postfix increment operator(i++) is generally more efficient than prefix increment operator(++i) when used in a loop or when the value of i is not needed immediately after the increment.
+    //postfix decrement operator(i--) is generally more efficient than prefix decrement operator(--i) when used in a loop or when the value of i is not needed immediately after the decrement.
+    */ 
+return 0;
+}
