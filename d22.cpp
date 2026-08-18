05142cdc7bd47fa0e38d70ba31f584a4c0f3133b
rohit9414 <rohit6201164441@gmail.com>
2026-08-18 11:03:05 +0000

    update

--- d22.cpp
+++ d22.cpp
@@ -9,4 +9,31 @@
 //      cout<<"maximum no : "<<maxof(a,b,c)<<endl;
 // }
 
+// whether a number is a polindrom or not
+// #include<iostream>
+// using namespace std;
+ 
+// int reverse(int num){
+//     int temp;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+//     int reverse=0;
+//     while(num!=0){
+//          temp=num%10;
+//          reverse=reverse*10 + temp;
+//          num=num/10;
+//     }
+//     return reverse;
+// }
+// bool check_polindrom(int num){
+//    if(reverse(num)==num){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+// int main(){
+//     int num;
+//     cin>>num;
+//     cout<<check_polindrom(num)<<endl;
+//     return 0;
+// }
 
