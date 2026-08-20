46317b08a5c869c539020b9499059963c394ef71
rohit9414 <rohit6201164441@gmail.com>
2026-08-20 09:37:33 +0000

    update

---
+++ d24.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+//   int main(){
+//     int num =8;
+//     cout<<(num>>2)<<endl;// right shift.
+//     cout<<(num<<2)<<endl;//lsft shift
+   
+//     return 0;
+// }
+
+
+//pointer 
+//  void swap(int* a,int* b){//pass by reference
+//        int temp;
+//        cout<<*a<<" "<<*b<<" ";
+//        temp=*a;
+//        *a=*b;
+//        *b=temp;
+//        cout<<*a<<" "<<*b<<" ";
+//  }
+
+//pass by reference method 2
+//  void swap(int &a,int &b){//pass by reference
+//        int temp;
+//        cout<<a<<" "<<b<<" ";
+//        temp=a;
+//        a=b;
+//        b=temp;
+//        cout<<a<<" "<<b<<" ";
+//  }
+
+
+// pass by value.
+// void swap(int a,int b){
+//     int temp;
+//     cout<<a<<" "<<b<<" ";
+//     temp=a;
+//     a=b;
+//     b=temp;
+//     cout<<a<<" "<<b<<" ";
+// }
+
+
+ int main(){
+    // int marks =10;
+    // int * ptr= &marks;// & operator used to store the addresses of the variable marks.
+    // cout<<ptr<<endl;//contain address of the variable.
+    // cout<<marks<<endl;
+    // cout<<*ptr<<endl;//* used to dereference , that means accessing the the value of the variavble using ptr  
+    // *ptr=20;// changing the value of thye variabe.
+    // cout<<marks<<endl;
+    // cout<<&ptr<<endl; 
+    // int **ptr1=&ptr;
+    // cout<<*ptr<<endl;
+    // cout<<**ptr1<<endl;
+
+
+    // int a=10;
+    // int b=20;
+    // // swap(&a,&b);//pass by reference.
+    // swap(a,b);
+    // cout<<" "<<a<<" "<<b<<endl;//pass by value.
+    
+    // swap(a,b);
+    // cout<<" "<<a<<" "<<b<<endl;//pass by reference using & , method 2.
+}
