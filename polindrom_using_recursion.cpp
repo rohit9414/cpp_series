720d98167d13fe17f99c68995fbdafe82d823104
rohit9414 <rohit6201164441@gmail.com>
2026-08-27 06:14:01 +0000

    polindrom using recursion

---
+++ polindrom_using_recursion.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+ string polin(string str,int srt,int end){
+       if(srt>=end){
+        return "a polindrom";
+       }
+    if(str[srt]!=str[end]){
+        
+        return "not a polindrom";
+    }
+    return polin(str,++srt,--end);//prepost hoga pahle increament the pass the value
+ }
+int main(){
+  string str="1221";
+  int len=str.length()-1;
+  string ans=polin(str,0,len);
+  cout<<ans<<endl;
+}
