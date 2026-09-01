321e1b4b1cf6dd131e17684c823815ca08ef0951
rohit9414 <rohit6201164441@gmail.com>
2026-09-01 06:14:54 +0000

    update

--- count_target_sum.cpp
+++ count_target_sum.cpp
@@ -1,23 +1,22 @@
 #include<iostream>
 using namespace std;
 
-void  sum_of_subsequnece(int arr[],int sum,int n,int idx,int *count,int tg){
-       
-        if(idx==n){
-          if(sum==tg){
-            (*count)++; 
+int count_sum_of_subsequnece(int arr[],int n,int idx,int tg){
+        if(tg==0){
+             return 1;
         }
-        return;
+        if(idx==n||tg<0){
+         return 0;
+        
         }
-        sum_of_subsequnece(arr,sum,n,idx+1,count,tg);//count already pointer ka address rakha hai isliye count ke saath yaha & nahi use karenge
-        sum_of_subsequnece(arr,sum+arr[idx],n,idx+1,count,tg);
+       return count_sum_of_subsequnece(arr,n,idx+1,tg) + count_sum_of_subsequnece(arr,n,idx+1,tg-arr[idx]);
+        
     }
 int main(){
     int arr[]={1,2,3,6,4,7};
     int sum=0;
     int idx=0;
-    int count=0;
     int target=8;
-    sum_of_subsequnece(arr,sum,6,idx,&count,target);
-    cout<<count<<endl;
+    
+    cout<<count_sum_of_subsequnece(arr,6,idx,target)<<endl;
 }
