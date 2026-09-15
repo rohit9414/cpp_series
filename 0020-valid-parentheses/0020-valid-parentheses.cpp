00e606ba0126997b7749406001481e8449c64283
Rohit Kumar goswami <vijay_2300523@gniot.net.in>
2026-09-15 12:31:13 +0530

    Time: 3 ms (9.24%), Space: 8.9 MB (37.12%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    bool isValid(string s) {
+      stack<char>st;
+      for(int i=0;i<s.size();i++)
+      {
+        if(s[i]=='('||s[i]=='{'||s[i]=='[')
+        {
+            st.push(s[i]);
+        }
+        else
+        {   if(st.empty())
+                {
+                    return false;
+                }
+            if(s[i]==')')
+            {
+                if(st.top()!='(')
+                {
+                    return false;
+                }
+                else
+                {
+                  st.pop();
+                }
+            }
+            else if(s[i]==']')
+            {   if(st.empty())
+                {
+                    return false;
+                }
+                if(st.top()!='[')
+                {
+                    return false;
+                }
+                else
+                {
+                    st.pop();
+                }
+            }
+            else
+            {   if(st.empty())
+                {
+                    return false;
+                }
+                if(st.top()!='{')
+                {
+                    return false;
+                }
+                else
+                {
+                    st.pop();
+                }
+            }
+        }
+      }
+      if(!st.empty())
+      {
+        return false;
+      }
+      else
+      {
+        return true;
+      }  
+    }
+};
