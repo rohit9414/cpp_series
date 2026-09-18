7383fd819ed95f90cfd7478460b8aacfd98bd35b
Rohit Kumar goswami <vijay_2300523@gniot.net.in>
2026-09-18 15:07:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Two Stacks in Array/two-stacks-in-array.cpp
@@ -0,0 +1,59 @@
+class twoStacks {
+    int arr[100];
+    int size = 100;
+    int top1, top2;
+
+  public:
+    twoStacks() {
+        // Initialize the top pointers of both stacks
+        top1=-1;
+        top2=100;
+        
+    }
+
+    void push1(int x) {
+        // Insert the given element at the top of the first stack
+        if(top1+1==top2)
+         return;
+          top1++;
+          arr[top1]=x;
+        
+    }
+
+    void push2(int x) {
+        // Insert the given element at the top of the second stack
+        if(top2-1==top1)
+        return;
+          top2--;
+          arr[top2]=x;
+        
+    }
+
+    int pop1() {
+        // Remove and return the top element of the first stack
+        // Return -1 if the stack is empty
+        if(top1==-1)
+        {
+            return -1;
+        }
+        else
+        {   int ele=arr[top1];
+            top1--;
+            return ele;
+        }
+    }
+
+    int pop2() {
+        // Remove and return the top element of the second stack
+        // Return -1 if the stack is empty
+        if(top2==100)
+        {
+            return -1;
+        }
+        else
+        {   int ele=arr[top2];
+            top2++;
+            return ele;
+        }
+    }
+};
