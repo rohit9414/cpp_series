00e91e4cf1f263368ea28bf904d2fcd069103cb3
Rohit Kumar goswami <vijay_2300523@gniot.net.in>
2026-09-23 13:18:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Binary Tree/sum-of-binary-tree.cpp
@@ -0,0 +1,21 @@
+/* Tree Node Structure
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};*/
+
+class Solution {
+  public:
+    int sumBT(Node* root) {
+        // code here
+        if (root==NULL)
+        return 0;
+        return (root->data + sumBT(root->left) + sumBT(root->right));
+    }
+};
