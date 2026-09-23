409a60bfe21dc24d9229bca625bb43d513bd15bb
Rohit Kumar goswami <vijay_2300523@gniot.net.in>
2026-09-23 17:30:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Identical Trees/identical-trees.cpp
@@ -0,0 +1,27 @@
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x) {
+        data = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    bool isIdentical(Node* r1, Node* r2) {
+        // code here
+        if(r1==NULL && r2==NULL)
+        return 1;
+        if(r1!=NULL&&r2==NULL||r1==NULL&&r2!=NULL)
+        return 0;
+        if(r1->data!=r2->data)
+        return 0;
+        return isIdentical(r1->left,r2->left)&&isIdentical(r1->right,r2->right);
+    }
+};
