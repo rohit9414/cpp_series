ffad4f4b7de48a21241757388992d8b133d747b0
Rohit Kumar goswami <vijay_2300523@gniot.net.in>
2026-09-23 14:27:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Internal Nodes in Tree/count-internal-nodes-in-tree.cpp
@@ -0,0 +1,27 @@
+/* Binary Tree Node Structure
+class Node {
+    public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int countNonLeafNodes(Node* root) {
+        // Code here
+        if(root==NULL)return 0;
+        if(root->left==NULL&&root->right==NULL)
+        {
+            return 0;
+        }
+        
+        return countNonLeafNodes(root->left)+countNonLeafNodes(root->right)+1;
+    }
+};
